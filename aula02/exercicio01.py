03a9a00179baf7e31b815c48447138e2475af0a3
sheezarainbow <101677993+sheezarainbow9@users.noreply.github.com>
2023-02-16 21:24:39 -0300

    exercício

--- aula02/exercicio01.py
+++ aula02/exercicio01.py
@@ -1,4 +1,10 @@
-# Um cliente irá solicitar um empréstimo ao banco. Se o valor do empréstimo for menor ou igual a 50 % do seu salário, então o empréstimo será aprovado. Senão, se o valor do empréstimo for menor ou igual a 75 % do salário a situação ficará em análise. Senão, informe ao cliente que o empréstimo não foi aprovado. (ações: 1 – aprovar o empréstimo,  2 – situação em análise, 3 – não aprovar o empréstimo).
+# Um cliente irá solicitar um empréstimo ao banco.
+# Se o valor do empréstimo for menor ou igual a 50 % do seu salário,
+# então o empréstimo será aprovado. Senão, se o valor do empréstimo
+# for menor ou igual a 75 % do salário a situação ficará em análise.
+# Senão, informe ao cliente que o empréstimo não foi aprovado.
+# (ações: 1 – aprovar o empréstimo,  2 – situação em análise,
+# 3 – não aprovar o empréstimo).
 
 
 valor = float(input('Digite o valor do empréstimo: R$ '))
