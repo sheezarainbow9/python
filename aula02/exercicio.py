835612b3ccd648c3d5caee43d57dd287f0a11c3a
sheezarainbow <101677993+sheezarainbow9@users.noreply.github.com>
2023-02-20 10:52:41 -0300

    exercícios aula 02

---
+++ aula02/exercicio.py
@@ -0,0 +1,25 @@
+# Atividades da aula 02:
+
+# 1. Faça um programa que leia um número inteiro e
+# verifique se ele é um número primo. Um número é
+# primo se ele é divisível apenas por 1 e por ele mesmo.
+# Se o número for primo, o programa deve imprimir "É um
+# número primo", caso contrário, deve imprimir "Não é um número primo".
+
+# num = int(input('Digite um número: '))
+# cont = 0
+
+# for i in range(1, num+1):
+#   if num % i == 0:
+#     cont+=1
+    
+# if cont == 2:
+#   print(f"O número {num} é primo.")
+# else:
+#   print(f"O número {num} não é primo.")
+
+
+# 2 - Escreva um programa que recebe o nome e
+# a idade de uma pessoa e verifica se ela pode votar nas eleições.
+
+
