407328b343ff801720b71ca630bf7fb3ae889303
sheezarainbow <101677993+sheezarainbow9@users.noreply.github.com>
2023-02-28 19:26:26 -0300

    listas

---
+++ aula03/listas.py
@@ -0,0 +1,27 @@
+# Listas:
+# declarando uma lista vazia em Python
+#lista1 = []
+
+# criando lista
+lista1 = [1, '2', 3]
+#print(type(lista1), lista1)
+
+# declaração explícita de lista
+lista2 = list((1, '2', 3))
+#print(lista2)
+
+# declaração implícita de lista
+lista3 = ['C', 4.65, True, 'True', 'Vamos aprender', ['outra', 'lista', 'interna'], lista2]
+#print(lista3)
+
+lista4 = ['primeiro', 'segundo', 'terceiro']
+#print(lista4)
+
+# acessando um elemento da lista
+#print(lista3)
+#print('')
+#print(lista3[5][0]) # acessando lista dentro da lista
+
+# fatiando listas:
+# nomeDaLista[start:stop:step]
+# execute um print por vez!
