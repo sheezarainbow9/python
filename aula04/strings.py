060b569ca9da772f2665191b22db61e3001518fd
sheezarainbow <101677993+sheezarainbow9@users.noreply.github.com>
2023-03-02 20:13:55 -0300

    strings

---
+++ aula04/strings.py
@@ -0,0 +1,19 @@
+# Strings:
+
+# declarando uma string
+msg = 'Hello, World!'
+print(msg)
+
+# concatenando string
+primeiro_nome = 'João'
+sobrenome = 'Silva'
+print(primeiro_nome + ' ' + sobrenome)
+
+# acessando caracteres individuais numa string
+primeiro_caractere = msg[0]
+ultimo_caractere = msg[-1]
+print(primeiro_caractere)
+print(ultimo_caractere)
+
+# comprimento de uma string
+print(f'comprimento = ', len(msg))
