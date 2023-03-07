804a1aab51e1b8e8a26e2b1cb20c43137076106d
sheezarainbow <101677993+sheezarainbow9@users.noreply.github.com>
2023-03-07 19:46:04 -0300

    range, for, while

---
+++ aula05/range.py
@@ -0,0 +1,48 @@
+# Range:
+
+# gerando uma sequência de 0 a 9
+# print(list(range(10)))
+
+# gerando uma sequência de 1 a 10
+# print(list(range(1, 11)))
+
+# gerando uma sequência de 0 a 30 com step 5
+# print(list(range(0, 30, 5)))
+
+# gerando uma sequência numérica negativa
+# print(list(range(0, -10, -1)))
+
+# Iteração e laços(repetição):
+
+# for i in range(1, 6):
+#   print(i)
+
+# i = 1
+# while i <= 5:
+#   print(i)
+#   i += 1
+
+# For:
+# produtos = []
+# valores = []
+
+# for i in range(0, 4):
+#   p = str(input('Digite o nome do produto: '))
+#   v = float(input('Digite o valor do produto: R$ '))
+#   produtos.append(p)
+#   valores.append(v)
+
+# print(f'Produtos: {produtos}\nValores: {valores}')
+
+# percorrendo lista de números
+# for elemento in [1, 2, 3, 4, 5, 6]:
+#   print('Estamos no elemento', elemento)
+
+# percorrendo uma string
+# for elemento in 'STRING':
+  # print('Estamos no elemento', elemento)
+
+# percorrendo os índices de uma string
+# for elemento in range(len('STRING')):
+#   print('Estamos no elemento', elemento)
+
