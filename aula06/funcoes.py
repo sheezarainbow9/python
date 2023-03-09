9fdb18a8779d11cb0248d30d5433a430de1e2510
sheezarainbow <101677993+sheezarainbow9@users.noreply.github.com>
2023-03-09 20:29:41 -0300

    functions

---
+++ aula06/funcoes.py
@@ -0,0 +1,23 @@
+# def soma(a, b):
+  # return a + b
+#   print(a + b)
+
+# soma(1, 2)
+# soma(1, 20)
+# resultado = soma(1, 2)
+# print(f'Resultado da soma: {resultado}')
+# print(f'Resultado da soma: {soma(1, 2)}')
+
+# def subtracao(a, b):
+#   return a - b
+
+# resultado = subtracao(9, 6)
+# print(f'Resultado da subtração: {resultado}')
+
+def num_par(a):
+  if a % 2 == 0:
+    print(f'{a} é PAR.')
+  else:
+    print(f'O número {a} é ÍMPAR.')
+
+num = num_par(4)
