0e3931be615b739041dc9db8c327c65ea89d49ec
sheezarainbow <101677993+sheezarainbow9@users.noreply.github.com>
2023-03-09 21:50:10 -0300

    prática

---
+++ aula06/pratica.py
@@ -0,0 +1,34 @@
+# def numeros(n):
+#   for i in range(n):
+#     i += 1
+#     print(str(i) * i)
+
+# n = int(input('Digite um número: '))
+# numeros(n)
+
+# def soma(a, b, c):
+#     s = a + b + c
+#     return s
+
+# a = int(input('Digite o 1° número: '))
+# b = int(input('Digite o 2° número: '))
+# c = int(input('Digite o 3° número: '))
+
+# print('A soma dos números =', soma(a, b, c))
+
+# def positivo(n):
+#   if n > 0:
+#     print('P')
+#   elif n == 0:
+#     print('N')
+#   else:
+#     print('Z')
+
+# positivo(int(input('Digite um número: ')))
+
+# def somaImposto(taxaImposto, custo):
+#   return (1 + taxaImposto/100) * custo
+
+# taxa = float(input('Digite a taxa de imposto: '))
+# custo = float(input('Digite o custo: R$ '))
+# print(f'Valor com imposto: R${somaImposto(taxa, custo):.2f}')
