34dc67ab3cf93e916a43bb473a9c950141d54648
sheezarainbow <101677993+sheezarainbow9@users.noreply.github.com>
2023-03-14 20:20:41 -0300

    funções

--- aula07/funcoes.py
+++ aula07/funcoes.py
@@ -1,9 +1,10 @@
 # O 'self' indica que pertence à classe 'Pessoa'.
 
 class Pessoa:
-    def __init__(self, nome, idade):
+    def __init__(self, nome, idade, distancia):
         self.nome = nome
         self.idade = idade
+        self.distancia = distancia
 
     # def apresentar(self):
     #     print(f'Olá, meu nome é {self.nome} e tenho {self.idade} anos.')
@@ -11,10 +12,18 @@ class Pessoa:
     def aniversario(self):
       self.idade += 1
 
-pessoa = Pessoa('Maria', 36)
-pessoa.aniversario()
+    def passos(self):
+        self.distancia 
+        
 
-pessoa2 = Pessoa('João', 40)
+pessoa = Pessoa('Maria', 36, 5)
+pessoa.aniversario()
+pessoa.passos()
+
+pessoa2 = Pessoa('João', 40, 6)
 pessoa2.aniversario()
+pessoa2.passos()
 
 print(f'{pessoa2.nome} tem {pessoa2.idade} anos e {pessoa.nome} tem {pessoa.idade}.')
+print(f'{pessoa2.nome} andou {pessoa2.distancia}km hoje.')
+print(f'{pessoa.nome} andou {pessoa.distancia}km hoje.')
