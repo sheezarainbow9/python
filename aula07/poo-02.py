a4062a8914cf5049556585ec0bb34e5587dfb9d2
sheezarainbow <101677993+sheezarainbow9@users.noreply.github.com>
2023-03-16 19:14:39 -0300

    poo

---
+++ aula07/poo-02.py
@@ -0,0 +1,15 @@
+# Super() -> traz os métodos da classe pai para a classe filho.
+
+class Carro:
+    def __init__(self, nome) -> None:
+        self.nome = nome
+
+    def fazer_som(self):
+        print(f'{self.nome} faz bibi!')
+
+class Fusca(Carro):
+  def fazer(self):
+    super().fazer_som()
+
+teste = Fusca('Bob')
+teste.fazer()
